d628f3f2290c3befc7cdb21765455304d095bf29
Justin van der Kruit <justin.vanderkruit@tacstone.nl>
2023-12-01 13:55:50 +0100

    finished part 2

--- 2023/src/solutions/day_01.rs
+++ 2023/src/solutions/day_01.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use aoc_common::{Solution, load};
 use itertools::Itertools;
 use regex::Regex;
@@ -12,29 +14,60 @@ impl Solution for Day01 {
 
     fn part_1(&self) -> String {
         let input = load("day_01");
-        let regex = Regex::new(r"\d").unwrap();
         let mut calibration_values: Vec<isize> = Vec::new();
 
         for line in input.lines() {
-            let numbers = regex.find_iter(line)
-                .map(|f| f.as_str())
-                .collect_vec();
-
-            let mut first_numer = numbers.first().unwrap().to_string();
-            let second_numer = numbers.last().unwrap();
-
-            first_numer.push_str(second_numer);
-            calibration_values.push(first_numer.parse::<isize>().unwrap())
+            let first_last_number = find_first_last_number(line);
+            calibration_values.push(first_last_number.parse::<isize>().unwrap())
         }        
 
-        let sum: isize = calibration_values.iter()
-            .sum();
-        
+        let sum: isize = calibration_values.iter().sum();
         sum.to_string()
     }
 
     fn part_2(&self) -> String {
-        todo!()
+        let input = load("day_01");
+        let mut calibration_values: Vec<isize> = Vec::new();
+
+        for line in input.lines() {
+            let replaced_text_numbers = text_to_number(line);
+            let first_last_number = find_first_last_number(line);
+            calibration_values.push(first_last_number.parse::<isize>().unwrap())
+        }
+
+        let sum: isize = calibration_values.iter().sum();
+        sum.to_string()
     }
 }
 
+fn find_first_last_number(input: &str) -> String {
+    let regex = Regex::new(r"\d").unwrap();
+    let numbers = regex.find_iter(line)
+        .map(|f| f.as_str())
+        .collect_vec();
+
+    let mut first_numer = numbers.first().unwrap().to_string();
+    let second_numer = numbers.last().unwrap();
+
+    first_numer.push_str(second_numer);
+    first_numer
+}
+
+fn text_to_number(&mut input: &str) -> String {
+    let text_number = HashMap::from([
+        ("zero", 0),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8), 
+        ("nine", 9),
+    ]);
+
+    for (key, value) in text_number.into_iter() {
+        input.replace(key, value);
+    }
+}
